10b6b378f354438aaadfb900ecb341c87a2eef6a
vanessascarin <vanessa.hb.scarin@gmail.com>
2025-02-26 12:08:05 -0300

    Exemplo_25.05

---
+++ Exemplo_25.05/AreaTriangulo_Estruturado/AreaTriangulo_Estruturado/AreaTriangulo_Estruturado/Retangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AreaTriangulo_Estruturado
+{
+    class Retangulo
+    {
+        public double A;
+        public double B;
+
+        public double AreaRetangulo()
+        {
+            return A * B;
+        }
+        public double SemiPerimetro()
+        {
+            return (A + B) / 2;
+        }
+    }
+}
---
+++ Exemplo_25.05/AreaTriangulo_Estruturado/AreaTriangulo_Estruturado/AreaTriangulo_Estruturado/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaTriangulo_Estruturado
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+        public double p;
+
+        public double AreaTriangulo()
+        {
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            // return LadoA *LadoB;
+        }
+        public double SemiPerimetro()
+        {
+            return p = (A + B + C) / 2;
+        }
+    }
+}
