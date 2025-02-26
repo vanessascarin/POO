b60b6f4775a303ce678d7c65ad3829f2de954e56
vanessascarin <vanessa.hb.scarin@gmail.com>
2025-02-26 15:47:28 -0300

    Exercício 3 começo

---
+++ 1.Exemplo_25.02/AreaTriangulo_Estruturado/AreaTriangulo_Estruturado/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AreaTriangulo_Estruturado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+            Retangulo r = new Retangulo();
+            double area;
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+            double q = x.SemiPerimetro();
+            area = x.AreaTriangulo();
+            Console.WriteLine("Área de X = " + area.ToString("F2"));
+            Console.WriteLine("Semi perímetro = " + q.ToString("F2"));
+            //         ############### RETANGULO ####################
+            Console.WriteLine("Entre com as medidas do RETANGULO R:");
+            r.A = double.Parse(Console.ReadLine());
+            r.B = double.Parse(Console.ReadLine());
+            area = r.AreaRetangulo();
+            q = r.SemiPerimetro();
+            Console.WriteLine("Área de r = " + area.ToString("F2"));
+            Console.WriteLine("Semi perímetro r = " + q.ToString("F2"));
+
+
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+    }
+}
